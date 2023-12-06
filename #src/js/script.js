fc11f5add56b5e5dde52a7984c1fcef9ed73c6d1
Hillel1408 <etemax@bk.ru>
2023-12-06 17:43:02 +0300

    Журнал Рыба

---
+++ #src/js/script.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".slider__swiper", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".slider__button-next",
+    prevEl: ".slider__button-prev",
+  },
+});
